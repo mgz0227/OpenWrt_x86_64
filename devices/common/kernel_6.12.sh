d30860fa839b99c3f5dd00fd80e1f8e7e72f229a
miaogongzi <miaogongzi0227@gmail.com>
2024-11-30 00:36:08 +0800

    Update kernel_6.12.sh

--- devices/common/kernel_6.12.sh
+++ devices/common/kernel_6.12.sh
@@ -1,7 +1,9 @@
 shopt -s extglob
 
-rm -rf include
-rm -rf package
-rm -rf target/linux/generic
-rm -rf /target/linux/x86
-mv -rf devices/x86_64/files/* /
+rm -rf include/*
+rm -rf package/*
+rm -rf target/linux/generic/*
+rm -rf target/linux/x86/*
+mv -rf devices/x86_64/files/include/* include
+mv -rf devices/x86_64/files/package/* package
+mv -rf devices/x86_64/files/target/linux/generic/* target/linux/generic
