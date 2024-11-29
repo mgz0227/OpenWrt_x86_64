ff4186fd0c4ae0efedc05171be9796474bb9c10e
miaogongzi <miaogongzi0227@gmail.com>
2024-11-30 01:13:06 +0800

    Update kernel_6.12.sh

--- devices/common/kernel_6.12.sh
+++ devices/common/kernel_6.12.sh
@@ -4,9 +4,10 @@ rm -rf include/*
 rm -rf package/*
 rm -rf target/linux/generic/*
 rm -rf target/linux/x86/*
-
+rm -rf target/linux/x86/Makefile
 git_clone_path 6.12 https://github.com/namiltd/openwrt include include
 git_clone_path 6.12 https://github.com/namiltd/openwrt package package
 git_clone_path 6.12 https://github.com/namiltd/openwrt target/linux/generic target/linux/generic
 git_clone_path 6.12 https://github.com/namiltd/openwrt target/linux/x86 target/linux/x86
-wget -N https://raw.githubusercontent.com/mgz0227/Openwrt_6.12/main/target/linux/x86/Makefile -P include/target/linux/x86
+wget -N https://raw.githubusercontent.com/mgz0227/Openwrt_6.12/main/target/linux/x86/Makefile -P target/linux/x86
+cat target/linux/x86/Makefile
