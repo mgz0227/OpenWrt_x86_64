1f04ececbff5d5bac5aabe242d8c3ef177d6ebc3
miaogongzi <miaogongzi0227@gmail.com>
2024-11-30 18:14:43 +0800

    Update kernel_6.12.sh

--- devices/common/kernel_6.12.sh
+++ devices/common/kernel_6.12.sh
@@ -16,7 +16,7 @@ git_clone_path 6.12 https://github.com/namiltd/openwrt target/linux/generic targ
 git_clone_path 6.12 https://github.com/namiltd/openwrt target/linux/x86 target/linux/x86
 git_clone_path 6.12 https://github.com/namiltd/openwrt package/kernel/linux/modules package/kernel/linux/modules
 
-#wget -N https://raw.githubusercontent.com/openwrt/openwrt/main/package/devel/perf/Makefile -P package/devel/perf
+wget -N https://raw.githubusercontent.com/openwrt/openwrt/openwrt-23.05/package/devel/perf/Makefile -P package/devel/perf
 
 wget -N https://raw.githubusercontent.com/mgz0227/Openwrt_6.12/main/target/linux/x86/Makefile -P target/linux/x86
 wget -N https://raw.githubusercontent.com/namiltd/openwrt/6.12/package/kernel/gpio-button-hotplug/src/gpio-button-hotplug.c -P package/kernel/gpio-button-hotplug/src
