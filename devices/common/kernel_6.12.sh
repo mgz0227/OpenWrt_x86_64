3d226898480e6f82e75c6dde1fc0cff2438ae734
miaogongzi <miaogongzi0227@gmail.com>
2024-12-02 21:22:23 +0800

    Update kernel_6.12.sh

--- devices/common/kernel_6.12.sh
+++ devices/common/kernel_6.12.sh
@@ -1,16 +1,6 @@
 shopt -s extglob
 
-rm -rf include/*
 
-rm -rf target/linux/generic/*
-rm -rf target/linux/x86/*
-
-rm -rf package/kernel/nat46
-rm -rf package/kernel/nat46/Makefile
-
-
-rm -rf package/devel/perf/*
-rm -rf package/devel/perf
 
 wget -N https://patch-diff.githubusercontent.com/raw/openwrt/openwrt/pull/16547.patch  -P devices/common/patches/
 
@@ -19,13 +9,7 @@ wget -N https://raw.githubusercontent.com/mgz0227/Openwrt_6.12/main/target/linux
 
 wget -N https://raw.githubusercontent.com/namiltd/openwrt/6.12/include/kernel-6.12 -P include
 
-cat target/linux/x86/Makefile
 
-cd feeds/packages
-
-git_clone_path main https://github.com/openwrt/openwrt libs/libtraceevent
-
-cd ../../
 
 sed -i '/$(curdir)\/compile:/c\$(curdir)/compile: package/opkg/host/compile' package/Makefile
 
